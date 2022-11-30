de97dfcff5a8246c22c2949d23520bd8bdbb09c8
Tamal Saha <tamal@appscode.com>
2022-12-01 01:01:48 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,5 +1,90 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/fluxcd/source-controller/api/v1beta2"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
+)
 
+func NewClient() (client.Client, error) {
+	scheme := runtime.NewScheme()
+	_ = clientgoscheme.AddToScheme(scheme)
+	_ = v1beta2.AddToScheme(scheme)
+
+	ctrl.SetLogger(klogr.New())
+	cfg := ctrl.GetConfigOrDie()
+	cfg.QPS = 100
+	cfg.Burst = 100
+
+	mapper, err := apiutil.NewDynamicRESTMapper(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return client.New(cfg, client.Options{
+		Scheme: scheme,
+		Mapper: mapper,
+		//Opts: client.WarningHandlerOptions{
+		//	SuppressWarnings:   false,
+		//	AllowDuplicateLogs: false,
+		//},
+	})
+}
+
+func main() {
+	if err := useGeneratedClient(); err != nil {
+		panic(err)
+	}
+	if err := useKubebuilderClient(); err != nil {
+		panic(err)
+	}
+}
+
+func useGeneratedClient() error {
+	fmt.Println("Using Generated client")
+	cfg := ctrl.GetConfigOrDie()
+	cfg.QPS = 100
+	cfg.Burst = 100
+
+	kc, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	var nodes *corev1.NodeList
+	nodes, err = kc.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	for _, db := range nodes.Items {
+		fmt.Println(client.ObjectKeyFromObject(&db))
+	}
+	return nil
+}
+
+func useKubebuilderClient() error {
+	fmt.Println("Using kubebuilder client")
+	kc, err := NewClient()
+	if err != nil {
+		return err
+	}
+
+	var nodes corev1.NodeList
+	err = kc.List(context.TODO(), &nodes)
+	if err != nil {
+		return err
+	}
+	for _, db := range nodes.Items {
+		fmt.Println(client.ObjectKeyFromObject(&db))
+	}
+	return nil
 }
